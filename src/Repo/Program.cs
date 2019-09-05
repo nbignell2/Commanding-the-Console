b0ea3019269e4b84df1cb0c331a2a8cdc59bee0e
nbignell2 <46579584+nbignell2@users.noreply.github.com>
2019-09-05 14:46:48 -0600

    Closes #2

---
+++ src/Repo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http; // for HttpClient
+using static System.Console; // Allows me to directly call static methods on the Console class, without the classname qualifier
+
+namespace Repo
+{
+    class Program
+    {
+        const string API_BASE = "https://gitignore.io/api/";
+        const string API_LINES = API_BASE + "list?format=lines";
+
+        static void Main(string[] args)
+        {
+            // Use the HttpClient to grab content from a website
+            var client = new HttpClient();
+            var response = client.GetAsync(API_LINES,HttpCompletionOption.ResponseContentRead).GetAwaiter().GetResult();
+            var templateLines = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            WriteLine(templateLines);
+        }
+    }
+}
